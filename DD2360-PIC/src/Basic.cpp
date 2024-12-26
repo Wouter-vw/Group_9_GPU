ce9a864cb490809d66a3e5b5fc0d66bd4cbbf267
Alam Sanchez <asancheztorres1905@gmail.com>
2024-12-26 13:54:37 -0600

    Remove register keywords

--- DD2360-PIC/src/Basic.cpp
+++ DD2360-PIC/src/Basic.cpp
@@ -8,7 +8,7 @@
 double dotP(FPfield *vect1, FPfield *vect2, int n) {
   double result = 0;
   double local_result = 0;
-  for (register int i = 0; i < n; i++) local_result += vect1[i] * vect2[i];
+  for (int i = 0; i < n; i++) local_result += vect1[i] * vect2[i];
   result = local_result;
   return (result);
 }
@@ -69,7 +69,7 @@ double norm2P(FPfield *vect, int n) {
 double normP(FPfield *vect, int n) {
   FPfield result = 0.0;
   FPfield local_result = 0.0;
-  for (register int i = 0; i < n; i++) local_result += vect[i] * vect[i];
+  for (int i = 0; i < n; i++) local_result += vect[i] * vect[i];
 
   // MPI_Allreduce(&local_result, &result, 1, MPI_DOUBLE, MPI_SUM,
   // MPI_COMM_WORLD);
@@ -78,51 +78,51 @@ double normP(FPfield *vect, int n) {
 }
 /** method to calculate the difference of two vectors*/
 void sub(FPfield *res, FPfield *vect1, FPfield *vect2, int n) {
-  for (register int i = 0; i < n; i++) res[i] = vect1[i] - vect2[i];
+  for (int i = 0; i < n; i++) res[i] = vect1[i] - vect2[i];
 }
 /** method to calculate the sum of two vectors vector1 = vector1 + vector2*/
 void sum(FPfield *vect1, FPfield *vect2, int n) {
-  for (register int i = 0; i < n; i++) vect1[i] += vect2[i];
+  for (int i = 0; i < n; i++) vect1[i] += vect2[i];
 }
 /** method to calculate the sum of two vectors vector1 = vector1 + vector2*/
 void sum(FPfield ***vect1, FPfield ***vect2, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect1[i][j][k] += vect2[i][j][k];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect1[i][j][k] += vect2[i][j][k];
 }
 
 /** method to calculate the sum of two vectors vector1 = vector1 + vector2*/
 void sum(FPfield ***vect1, FPfield ***vect2, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect1[i][j][0] += vect2[i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect1[i][j][0] += vect2[i][j][0];
 }
 
 /** method to calculate the sum of two vectors vector1 = vector1 + vector2*/
 void sum(FPfield ***vect1, FPfield ****vect2, int nx, int ny, int nz, int ns) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] += vect2[ns][i][j][k];
 }
 
 /** method to calculate the sum of two vectors vector1 = vector1 + vector2*/
 void sum(FPfield ***vect1, FPfield ****vect2, int nx, int ny, int ns) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect1[i][j][0] += vect2[ns][i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect1[i][j][0] += vect2[ns][i][j][0];
 }
 /** method to calculate the subtraction of two vectors vector1 = vector1 -
  * vector2*/
 void sub(FPfield ***vect1, FPfield ***vect2, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect1[i][j][k] -= vect2[i][j][k];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect1[i][j][k] -= vect2[i][j][k];
 }
 
 /** method to calculate the subtraction of two vectors vector1 = vector1 -
  * vector2*/
 void sub(FPfield ***vect1, FPfield ***vect2, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect1[i][j][0] -= vect2[i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect1[i][j][0] -= vect2[i][j][0];
 }
 
 /** method to sum 4 vectors vector1 = alfa*vector1 + beta*vector2 +
@@ -130,29 +130,29 @@ void sub(FPfield ***vect1, FPfield ***vect2, int nx, int ny) {
 void sum4(double ***vect1, double alfa, double ***vect2, double beta,
           double ***vect3, double gamma, double ***vect4, double delta,
           double ***vect5, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] =
             alfa * (vect2[i][j][k] + beta * vect3[i][j][k] +
                     gamma * vect4[i][j][k] + delta * vect5[i][j][k]);
 }
 /** method to calculate the scalar-vector product */
 void scale(FPfield *vect, FPfield alfa, int n) {
-  for (register int i = 0; i < n; i++) vect[i] *= alfa;
+  for (int i = 0; i < n; i++) vect[i] *= alfa;
 }
 
 /** method to calculate the scalar-vector product */
 void scale(FPfield ***vect, FPfield alfa, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect[i][j][0] *= alfa;
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect[i][j][0] *= alfa;
 }
 
 /** method to calculate the scalar-vector product */
 void scale(FPfield ***vect, FPfield alfa, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect[i][j][k] *= alfa;
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect[i][j][k] *= alfa;
 }
 /** method to calculate the scalar product */
 void scale(double vect[][2][2], double alfa, int nx, int ny, int nz) {
@@ -163,30 +163,30 @@ void scale(double vect[][2][2], double alfa, int nx, int ny, int nz) {
 /** method to calculate the scalar-vector product */
 void scale(FPfield ***vect1, FPfield ***vect2, FPfield alfa, int nx, int ny,
            int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = vect2[i][j][k] * alfa;
 }
 
 /** method to calculate the scalar-vector product */
 void scale(FPfield ***vect1, FPfield ***vect2, double alfa, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
       vect1[i][j][0] = vect2[i][j][0] * alfa;
 }
 
 /** method to calculate the scalar-vector product */
 void scale(FPfield *vect1, FPfield *vect2, double alfa, int n) {
-  for (register int i = 0; i < n; i++) vect1[i] = vect2[i] * alfa;
+  for (int i = 0; i < n; i++) vect1[i] = vect2[i] * alfa;
 }
 
 /** method to calculate vector1 = vector1 + alfa*vector2   */
 void addscale(FPfield alfa, FPfield ***vect1, FPfield ***vect2, int nx, int ny,
               int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = vect1[i][j][k] + alfa * vect2[i][j][k];
 }
 /** add scale for weights */
@@ -200,77 +200,77 @@ void addscale(double alfa, double vect1[][2][2], double vect2[][2][2], int nx,
 /** method to calculate vector1 = vector1 + alfa*vector2   */
 void addscale(FPfield alfa, FPfield ***vect1, FPfield ***vect2, int nx,
               int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
       vect1[i][j][0] += alfa * vect2[i][j][0];
 }
 /** method to calculate vector1 = vector1 + alfa*vector2   */
 void addscale(FPfield alfa, FPfield *vect1, FPfield *vect2, int n) {
-  for (register int i = 0; i < n; i++) vect1[i] += alfa * vect2[i];
+  for (int i = 0; i < n; i++) vect1[i] += alfa * vect2[i];
 }
 /** method to calculate vector1 = beta*vector1 + alfa*vector2   */
 void addscale(FPfield alfa, FPfield beta, FPfield *vect1, FPfield *vect2,
               int n) {
-  for (register int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
     vect1[i] = vect1[i] * beta + alfa * vect2[i];
 }
 /** method to calculate vector1 = beta*vector1 + alfa*vector2 */
 void addscale(FPfield alfa, FPfield beta, FPfield ***vect1, FPfield ***vect2,
               int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) {
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) {
         vect1[i][j][k] = beta * vect1[i][j][k] + alfa * vect2[i][j][k];
       }
 }
 /** method to calculate vector1 = beta*vector1 + alfa*vector2 */
 void addscale(double alfa, double beta, double ***vect1, double ***vect2,
               int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
       vect1[i][j][0] = beta * vect1[i][j][0] + alfa * vect2[i][j][0];
 }
 
 /** method to calculate vector1 = alfa*vector2 + beta*vector3 */
 void scaleandsum(double ***vect1, double alfa, double beta, double ***vect2,
                  double ***vect3, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = alfa * vect2[i][j][k] + beta * vect3[i][j][k];
 }
 /** method to calculate vector1 = alfa*vector2 + beta*vector3 with vector2
  * depending on species*/
 void scaleandsum(double ***vect1, double alfa, double beta, double ****vect2,
                  double ***vect3, int ns, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = alfa * vect2[ns][i][j][k] + beta * vect3[i][j][k];
 }
 /** method to calculate vector1 = alfa*vector2*vector3 with vector2 depending on
  * species*/
 void prod(double ***vect1, double alfa, double ****vect2, int ns,
           double ***vect3, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = alfa * vect2[ns][i][j][k] * vect3[i][j][k];
 }
 /** method to calculate vect1 = vect2/alfa */
 void div(double ***vect1, double alfa, double ***vect2, int nx, int ny,
          int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = vect2[i][j][k] / alfa;
 }
 void prod6(double ***vect1, double ***vect2, double ***vect3, double ***vect4,
            double ***vect5, double ***vect6, double ***vect7, int nx, int ny,
            int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = vect2[i][j][k] * vect3[i][j][k] +
                          vect4[i][j][k] * vect5[i][j][k] +
                          vect6[i][j][k] * vect7[i][j][k];
@@ -280,9 +280,9 @@ void proddiv(double ***vect1, double ***vect2, double alfa, double ***vect3,
              double ***vect4, double ***vect5, double ***vect6, double beta,
              double ***vect7, double ***vect8, double gamma, double ***vect9,
              int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++)
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++)
         vect1[i][j][k] = (vect2[i][j][k] +
                           alfa * (vect3[i][j][k] * vect4[i][j][k] -
                                   vect5[i][j][k] * vect6[i][j][k]) +
@@ -296,56 +296,56 @@ void proddiv(double ***vect1, double ***vect2, double alfa, double ***vect3,
 }
 /** method to calculate the opposite of a vector */
 void neg(FPfield ***vect, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect[i][j][k] = -vect[i][j][k];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect[i][j][k] = -vect[i][j][k];
 }
 
 /** method to calculate the opposite of a vector */
 void neg(FPfield ***vect, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect[i][j][0] = -vect[i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect[i][j][0] = -vect[i][j][0];
 }
 /** method to calculate the opposite of a vector */
 void neg(FPfield ***vect, int nx) {
-  for (register int i = 0; i < nx; i++) vect[i][0][0] = -vect[i][0][0];
+  for (int i = 0; i < nx; i++) vect[i][0][0] = -vect[i][0][0];
 }
 /** method to calculate the opposite of a vector */
 void neg(FPfield *vect, int n) {
-  for (register int i = 0; i < n; i++) vect[i] = -vect[i];
+  for (int i = 0; i < n; i++) vect[i] = -vect[i];
 }
 /** method to set equal two vectors */
 void eq(double ***vect1, double ***vect2, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect1[i][j][k] = vect2[i][j][k];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect1[i][j][k] = vect2[i][j][k];
 }
 /** method to set equal two vectors */
 void eq(double ***vect1, double ***vect2, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect1[i][j][0] = vect2[i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect1[i][j][0] = vect2[i][j][0];
 }
 
 /** method to set equal two vectors */
 void eq(double ****vect1, double ***vect2, int nx, int ny, int is) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect1[is][i][j][0] = vect2[i][j][0];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect1[is][i][j][0] = vect2[i][j][0];
 }
 /** method to set equal two vectors */
 void eq(double ****vect1, double ***vect2, int nx, int ny, int nz, int is) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect1[is][i][j][k] = vect2[i][j][k];
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect1[is][i][j][k] = vect2[i][j][k];
 }
 
 void eq(double *vect1, double *vect2, int n) {
-  for (register int i = 0; i < n; i++) vect1[i] = vect2[i];
+  for (int i = 0; i < n; i++) vect1[i] = vect2[i];
 }
 /** method to set a vector to a Value */
 void eqValue(FPfield value, FPfield ***vect, int nx, int ny, int nz) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++)
-      for (register int k = 0; k < nz; k++) vect[i][j][k] = value;
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++)
+      for (int k = 0; k < nz; k++) vect[i][j][k] = value;
 }
 void eqValue(double value, double vect[][2][2], int nx, int ny, int nz) {
   for (int i = 0; i < nx; i++)
@@ -354,16 +354,16 @@ void eqValue(double value, double vect[][2][2], int nx, int ny, int nz) {
 }
 /** method to set a vector to a Value */
 void eqValue(double value, double ***vect, int nx, int ny) {
-  for (register int i = 0; i < nx; i++)
-    for (register int j = 0; j < ny; j++) vect[i][j][0] = value;
+  for (int i = 0; i < nx; i++)
+    for (int j = 0; j < ny; j++) vect[i][j][0] = value;
 }
 /** method to set a vector to a Value */
 void eqValue(double value, double ***vect, int nx) {
-  for (register int i = 0; i < nx; i++) vect[i][0][0] = value;
+  for (int i = 0; i < nx; i++) vect[i][0][0] = value;
 }
 /** method to set a vector to a Value */
 void eqValue(FPfield value, FPfield *vect, int n) {
-  for (register int i = 0; i < n; i++) vect[i] = value;
+  for (int i = 0; i < n; i++) vect[i] = value;
 }
 /** method to put a column in a matrix 2D */
 void putColumn(FPfield **Matrix, FPfield *vect, int column, int n) {
