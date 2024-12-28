a1315462027dd5e48fefc09ed6a7fa296dbfaefb
Alam Sanchez <asancheztorres1905@gmail.com>
2024-12-27 21:39:39 -0600

    Update formatting

--- DD2360-PIC/include/EMfield.h
+++ DD2360-PIC/include/EMfield.h
@@ -4,27 +4,24 @@
 #include "Alloc.h"
 #include "Grid.h"
 
-
 /** structure with field information */
 struct EMfield {
-    // field arrays: 4D arrays
-    
-    /* Electric field defined on nodes: last index is component */
-    FPfield*** Ex;
-    FPfield* Ex_flat;
-    FPfield*** Ey;
-    FPfield* Ey_flat;
-    FPfield*** Ez;
-    FPfield* Ez_flat;
-    /* Magnetic field defined on nodes: last index is component */
-    FPfield*** Bxn;
-    FPfield* Bxn_flat;
-    FPfield*** Byn;
-    FPfield* Byn_flat;
-    FPfield*** Bzn;
-    FPfield* Bzn_flat;
-    
-    
+  // field arrays: 4D arrays
+
+  /* Electric field defined on nodes: last index is component */
+  FPfield*** Ex;
+  FPfield* Ex_flat;
+  FPfield*** Ey;
+  FPfield* Ey_flat;
+  FPfield*** Ez;
+  FPfield* Ez_flat;
+  /* Magnetic field defined on nodes: last index is component */
+  FPfield*** Bxn;
+  FPfield* Bxn_flat;
+  FPfield*** Byn;
+  FPfield* Byn_flat;
+  FPfield*** Bzn;
+  FPfield* Bzn_flat;
 };
 
 /** allocate electric and magnetic field */
